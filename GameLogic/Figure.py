a4a95c1c3b051d4987ae0db95c56b295b07d615e
Adiutant <lagynkov.rom@gmail.com>
2021-02-21 00:40:47 +0300

    in

--- GameLogic/Figure.py
+++ GameLogic/Figure.py
@@ -1,8 +1,7 @@
-
-
 class Figure:
     type = 1
     turnmatrix = []
+    ally = []
     figures = {
         "1": "wP.png", "2": "bP.png", "3": "wK.png", "4": "bK.png", "5": "wQ.png",
         "6": "bQ.png",
@@ -15,7 +14,10 @@ class Figure:
             return 0
         return self.figures[str(self.type)]
 
-
-
     def __init__(self, typ):
         self.type = typ
+        if self.type % 2 != 0:
+            self.white = 1
+            self.ally = [1, 3, 5, 7, 9, 11]
+        else:
+            self.ally = [2, 4, 6, 8, 10, 12]
